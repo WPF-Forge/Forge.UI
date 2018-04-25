3d060e408e1f49672ad88a08e38fc354ecdc0f88
redbaty <juninhowblu@gmail.com>
2018-04-25 17:56:11 -0300

    Added ActionDescription

---
+++ Forge.UI/Models/ActionDescription.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace Forge.UI.Models
+{
+    public class ActionDescription
+    {
+        public ActionDescription(JProperty jProperty)
+        {
+            Name = jProperty.Name;
+            Actions.AddRange(jProperty.Value.Values<dynamic>());
+        }
+
+        public string Name { get; set; }
+
+        public List<dynamic> Actions { get; set; } = new List<dynamic>();
+    }
+}

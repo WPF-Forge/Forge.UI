ed43662b7e0af2589128b1cd8a5c56d60ac2eb58
Marcos Vinicius Maia <juninhowblu@gmail.com>
2018-04-25 22:11:01 -0300

    Added ActionCollection

---
+++ Forge.UI/Interfaces/ActionCollection.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Forge.UI.Interfaces
+{
+    internal class ActionCollection : Dictionary<string, List<IAction>>, IActionCollection
+    {
+        public void AddAction(IAction action)
+        {
+            AddOrSet(action.Key, action);
+        }
+
+        public void AddOrSet(string key, IAction value)
+        {
+            if (ContainsKey(key))
+            {
+                this[key].Add(value);
+            }
+            else
+            {
+                this[key] = new List<IAction> {value};
+            }
+        }
+
+        public void AddOrSet(string key, IEnumerable<IAction> valueEnumerable)
+        {
+            foreach (var item in valueEnumerable)
+            {
+                AddOrSet(key, item);
+            }
+        }
+
+        public void FromLookup(ILookup<string, IAction> lookup)
+        {
+            foreach (var grouping in lookup)
+            {
+                AddOrSet(grouping.Key, grouping);
+            }
+        }
+    }
+}
---
+++ Forge.UI/Interfaces/IActionCollection.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Forge.UI.Interfaces
+{
+    public interface IActionCollection : IDictionary<string, List<IAction>>
+    {
+        void AddAction(IAction action);
+        void AddOrSet(string key, IAction value);
+        void AddOrSet(string key, IEnumerable<IAction> valueEnumerable);
+        void FromLookup(ILookup<string, IAction> lookup);
+    }
+}

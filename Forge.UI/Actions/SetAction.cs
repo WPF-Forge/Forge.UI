6248173ad59e123ef4bb181d96fc676ec822d05e
Marcos Vinicius Maia <juninhowblu@gmail.com>
2018-04-25 22:12:49 -0300

    Added SetAction implementation

---
+++ Forge.UI/Actions/SetAction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Forge.UI.Interfaces;
+
+namespace Forge.UI.Actions
+{
+    public class SetAction : IAction
+    {
+        public string Key { get; } = "set";
+        public Action<object[]> Action { get; } = DoAction;
+
+        private static void DoAction(object[] obj)
+        {
+            
+        }
+    }
+}
